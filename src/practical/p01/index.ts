35a7f655b90aea50b67cc529bd79fca791cb4f73
Surajeeth Taladtanee <68011174@kmitl.ac.th>
2026-01-16 10:20:03 +0700

    hekaokoa

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,34 @@
-export function getEdgePosts() {}
+import axios from 'axios';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface EdgePost {
+  id: number;
+  title: string;
+}
+
+export const getEdgePosts = async (): Promise<EdgePost[]> => {
+  try {
+    const { data } = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+
+    if (data.length === 0) {
+      return [];
+    }
+
+    const firstPost = data[0];
+    const lastPost = data[data.length - 1];
+
+    
+    return [firstPost, lastPost].map(({ id, title }) => ({
+      id,
+      title,
+    }));
+  } catch (error) {
+    throw error;
+  }
+};
--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,31 @@
-export function getPostsByUser() {}
+import axios from 'axios';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface UserPost {
+  id: number;
+  title: string;
+}
+
+export const getPostsByUser = async (userId: number): Promise<UserPost[]> => {
+  try {
+    const { data } = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+
+    
+    return data
+      .filter((post) => post.userId === userId)
+      .map(({ id, title }) => ({
+        id,
+        title,
+      }));
+
+  } catch (error) {
+    throw error;
+  }
+};
+
